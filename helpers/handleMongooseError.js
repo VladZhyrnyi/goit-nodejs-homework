8fb611de0ae855b20675732b0d14c360ecde4c75
VladZhyrnyi <vladzhyrnyi@ukr.net>
2023-06-19 20:06:12 +0300

    modify handleMongooseError

--- helpers/handleMongooseError.js
+++ helpers/handleMongooseError.js
@@ -1,5 +1,14 @@
+const ERRORS = {
+  11000: 409,
+};
+
 const handleMongooseError = (error, data, next) => {
+  const { name, code } = error;
   error.status = 400;
+
+  if (name === "MongoServerError") {
+    error.status = ERRORS[+code] || 400;
+  }
   next();
 };
 
