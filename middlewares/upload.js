1dc23bdd29044583204968c04171ca4bbf75fb15
VladZhyrnyi <vladzhyrnyi@ukr.net>
2023-06-27 21:41:33 +0300

    add upload middleware

---
+++ middlewares/upload.js
@@ -0,0 +1,12 @@
+const multer = require("multer");
+const path = require("path");
+
+const tempDir = path.join(__dirname, "../", "temp");
+
+const config = multer.diskStorage({
+  destination: tempDir,
+});
+
+const upload = multer({ storage: config });
+
+module.exports = upload;
